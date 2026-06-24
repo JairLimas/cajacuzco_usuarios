1c58e300aab0f55ec0e2b3e4acb11dde1a8fefac
Cajacuzco_admin <60010940@continental.edu.pe>
2026-06-23 19:46:46 -0500

    agregué login de usuarios

--- app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from app.routes.usuario import router_usuario
-
-app = FastAPI(
-    title="Banco Cuzco API",
-    version="1.0.0"
-)
-
-app.include_router(router_usuario)
